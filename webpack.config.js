e582b210d451bd4cfc6b851d62768da10ade095b
GGGGreatOne <283548875@qq.com>
2025-01-17 14:31:43 +0800

    feat: add config

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  plugins: [],
+  resolve: {
+    extensions: ['*', '.mjs', '.js', '.json']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.mjs$/,
+        include: /node_modules/,
+        type: 'javascript/auto'
+      }
+    ]
+  }
+}
